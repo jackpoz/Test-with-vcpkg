917bd60058ddb0f60fecb58c32c6314b68bb7a3b
jackpoz <giacomopoz@gmail.com>
2026-05-25 20:28:58 +0200

    Add initial setup

---
+++ Test with vcpkg.cpp
@@ -0,0 +1,12 @@
+﻿// Test with vcpkg.cpp : Defines the entry point for the application.
+//
+
+#include "Test with vcpkg.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
